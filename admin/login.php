f821a6a8d532a2066354c93817c2f1488c955f5a
Thuận Hà <hathuan699@gmail.com>
2022-07-05 10:41:05 +0700

    update-admid-login

--- admin/login.php
+++ admin/login.php
@@ -35,29 +35,41 @@ if(isset($_POST['submit'])){
       <link rel="stylesheet" href="assets/css/flag-icon.min.css">
       <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
       <link rel="stylesheet" href="assets/css/style.css">
+      <link rel="stylesheet" href="assets/css/stylesheet.css">
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
    </head>
-   <body class="bg-dark">
-      <div class="sufee-login d-flex align-content-center flex-wrap">
-         <div class="container">
-            <div class="login-content">
-               <div class="login-form mt-150">
+   <body>
+      <section>
+            <div class="imggg">
+               <img src="jhin.jpg">
+            </div>
+            <div class="content">
+               <div class="form">
+                  <h2>Login</h2>
                   <form method="post">
-                     <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" name="username" class="form-control" placeholder="Username" required>
+
+                     <div class="inputs">
+                        <span>Username</span>
+                        <input type="text" name="username" placeholder="Username" required>
                      </div>
-                     <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+
+                     <div class="inputs">
+                        <span>Password</span>
+                        <input type="password" name="password" placeholder="Password" required>
                      </div>
-                     <button type="submit" name="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Sign in</button>
-					</form>
-					<div class="field_error"><?php echo $msg?></div>
+                     <div class="remember">
+                           <label><input type="checkbox" name=""> Remember me</label>
+                     </div>
+
+                     <div class="inputs">
+                        <button type="submit" class="buttons" name="submit">Sign in</button>
+                     </div>              
+
+                  </form>
+                  <div class="field_error"><?php echo $msg?></div>
                </div>
             </div>
-         </div>
-      </div>
+         </section>
       <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
       <script src="assets/js/popper.min.js" type="text/javascript"></script>
       <script src="assets/js/plugins.js" type="text/javascript"></script>
